e8b7564ea9c3d14baa3acaf21a1368073852ab18
prazwal-bns <prajwalbns15@gmail.com>
2025-10-19 19:33:27 +0545

    Add configuration file for multi-source file upload settings

---
+++ config/multi-source-file-upload.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'poll_interval' => 50,
+
+    'labels' => [
+        'file_upload' => 'File Upload',
+        'url_upload' => 'URL Upload',
+    ],
+
+    'icons' => [
+        'file_upload' => 'heroicon-o-arrow-up-tray',
+        'url_upload' => 'heroicon-o-globe-alt',
+    ]
+];
